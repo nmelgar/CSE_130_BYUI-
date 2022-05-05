8c5909284cd12ccd80eee1703e0ca784ca7d38be
nmelgar <nmelgar16013@gmail.com>
2022-05-05 06:44:40 -0500

    Problem 6 solved.

---
+++ week3/problem6_linearsearch.py
@@ -0,0 +1,7 @@
+numbers = [2, 6, 12, 34, 46, 78, 111, 66, 9, 37]
+value_to_search = 13
+
+if value_to_search in numbers:
+    print(f"Number {value_to_search} is in list!")
+else:
+    print(f"Number {value_to_search} is not in list")
