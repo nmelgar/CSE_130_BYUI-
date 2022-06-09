c432d36778e0b198ff6e7eab17c2f5e1c47075f7
nmelgar <nmelgar16013@gmail.com>
2022-06-09 18:36:07 -0500

    Working with the sort algorithm, not working yet.

---
+++ week8/sort_try5.py
@@ -0,0 +1,20 @@
+def swapPositions(list, pos1, pos2):
+
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+
+
+array1 = [31, 72, 10, 32, 18, 95, 25, 50]
+
+minus = -1
+i_largest = 0
+for i_check in array1:
+    i_pivot = array1[minus]
+    if i_check > i_largest:
+        i_largest = i_check
+        if i_largest > i_pivot:
+            index1 = array1.index(i_largest)
+            index2 = array1.index(i_pivot)
+            swapPositions(array1, index1, index2)
+            # minus = minus - 1
+print(array1)
