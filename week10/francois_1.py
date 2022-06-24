b3438d01be79578d85d0f76c1e7ab0c2597ee499
nmelgar <nmelgar16013@gmail.com>
2022-06-23 22:52:32 -0500

    Starting with francois numerical sequence program.

---
+++ week10/francois_1.py
@@ -0,0 +1,39 @@
+
+# n1 = 2
+# n2 = 1
+array1 = [2, 1]
+
+for x in range(100):
+    n1 = array1[x]
+    n2 = array1[x + 1]
+    
+    new_number = n1 + n2
+    array1.append(new_number)
+    # new_number = array1[x - 1] + array1[x + 1]
+    # array1[x - 1] = array1[x+1]
+    # array1[x+1] = new_number
+    # 
+    # print(f"{x+1}. {new_number}")
+    # print(x)
+    x += 1
+
+print(array1)
+# print(f"New number: {new_number}. N1{n1}. N2 {n2}")
+
+
+####FIRST VERSION####
+
+# n1 = 2
+# n2 = 1
+# array1 = [2, 1]
+
+# for x in range(100 - 2):
+#     new_number = n1 + n2
+#     n1 = n2
+#     n2 = new_number
+
+#     print(f"{x+1}. {new_number}")
+#     x += 1
+
+
+# print(f"New number: {new_number}. N1{n1}. N2 {n2}")
---
+++ week10/problem1.py
@@ -0,0 +1,6 @@
+user_number = int(input("Please enter a number: "))
+
+total = 0
+for number in range(user_number + 1):
+    total += number
+print(total)
