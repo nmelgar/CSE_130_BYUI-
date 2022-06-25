dc8cdc75596c7c715932b4b6b1bfd6142c6c4917
nmelgar <nmelgar16013@gmail.com>
2022-06-24 23:19:32 -0500

    Done with numeric design.

--- week10/francois_1.py
+++ week10/francois_1.py
@@ -1,12 +1,13 @@
 array1 = [2, 1]
+assert len(array1) > 0, "Array cannot be empty"
 
 user_integer = int(input("Which Francois number would you like to see? "))
 assert user_integer > 0, "Integer must be greater than 0"
 
 if user_integer == 1:
-    print(f"Francois number of {user_integer} is {array1[0]}")
+    print(f"Francois number {user_integer} is {array1[0]}")
 elif user_integer == 2:
-    print(f"Francois number of {user_integer} is {array1[1]}")
+    print(f"Francois number {user_integer} is {array1[1]}")
 else:
     for x in range(user_integer):
         n1 = array1[x]
@@ -14,9 +15,30 @@ else:
 
         new_number = n1 + n2
         array1.append(new_number)
-        # print(f"{x+1}. {array1[x]}")
+
         x += 1
-    print(f"\nFrancois number of {user_integer} is {array1[x-1]}")
+    print(f"\nFrancois number {user_integer} is {array1[x-1]}")
+
+
+# array1 = [2, 1]
+
+# user_integer = int(input("Which Francois number would you like to see? "))
+# assert user_integer > 0, "Integer must be greater than 0"
+
+# if user_integer == 1:
+#     print(f"Francois number {user_integer} is {array1[0]}")
+# elif user_integer == 2:
+#     print(f"Francois number {user_integer} is {array1[1]}")
+# else:
+#     for x in range(user_integer):
+#         n1 = array1[x]
+#         n2 = array1[x + 1]
+
+#         new_number = n1 + n2
+#         array1.append(new_number)
+#         # print(f"{x+1}. {array1[x]}")
+#         x += 1
+#     print(f"\nFrancois number {user_integer} is {array1[x-1]}")
 
 
 ####FIRST VERSION####
